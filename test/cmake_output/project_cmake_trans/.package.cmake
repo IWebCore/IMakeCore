4916683bb2ecedd2d3d2fb8db2c7504b57446643
岳克远 <yuekeyuan001+iwebcore@gmail.com>
2026-07-25 11:22:29 +0800

    submit code

---
+++ test/cmake_output/project_cmake_trans/.package.cmake
@@ -0,0 +1,11 @@
+###################################
+# SYSTEM CONFIGURED, DO NOT EDIT!!!
+###################################
+
+# test@world@1.0.0
+# Test library that depends on hello
+include(C:/Users/Yue/IMakeCore/test/cmake_output/project_cmake_trans/.lib/test@world@1.0.0.cmake)
+
+# test@hello@2.0.0
+# Test source library v2 (contains .cpp)
+include(C:/Users/Yue/IMakeCore/test/cmake_output/project_cmake_trans/.lib/test@hello@2.0.0.cmake)
